096ca9260bf595b8f352558f4b1aa63f2983589b
Vinit <vinitsalve@25gmail.com>
2025-07-10 09:38:14 +0530

    Added the employee project

---
+++ employee/src/employee/Employee.java
@@ -0,0 +1,90 @@
+package employee;
+
+public class Employee {
+	public static void main(String[] args) {
+		EmployeeData ed=new EmployeeData();
+		ed.setEmpid(1234);
+		System.out.println(ed.getEmpid());
+		ed.setName("Vinit");
+		System.out.println(ed.getName());
+		ed.setDepartment("development");
+		System.out.println(ed.getDepartment());
+		ed.setSalary(15000);
+		System.out.println(ed.getSalary());
+		ed.setEmail("vinit@gmail.com");
+		System.out.println(ed.getEmail());
+		ed.setPhonenum(974756435);
+		System.out.println(ed.getPhonenum());
+		ed.setDesignation("Developer");
+		System.out.println(ed.getDesignation());
+	}
+
+}
+
+class EmployeeData{
+	private int empid;
+	private String name;
+	private String department;
+	private int salary;
+	private String email;
+	private int phoneNumber;
+	private String designation;
+	
+	void setEmpid(int e)
+	{
+		empid = e;
+	}
+	int getEmpid()
+	{
+		return empid;
+	}
+	void setName(String n)
+	{
+		name = n;
+	}
+	String getName()
+	{
+		return name;
+	}
+	void setDepartment(String d)
+	{
+		department = d;
+	}
+	String getDepartment()
+	{
+		return department;
+	}
+	void setSalary(int s)
+	{
+		salary = s;
+	}
+	int getSalary()
+	{
+		return salary;
+	}
+	void setEmail(String em)
+	{
+		email = em;
+	}
+	String getEmail()
+	{
+		return email;
+	}
+	void setPhonenum(int p)
+	{
+		phoneNumber = p;
+	}
+	int getPhonenum()
+	{
+		return phoneNumber;
+	}
+	void setDesignation(String d)
+	{
+		designation = d;
+	}
+	String getDesignation()
+	{
+		return designation;
+	}
+	
+}
